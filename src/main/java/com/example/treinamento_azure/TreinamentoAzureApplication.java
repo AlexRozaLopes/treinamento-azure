540c58d57628dcc464ac241373b0396303e88408
lopes <alexrozaestudos@gmail.com>
2026-01-28 18:08:36 -0300

    first commit

---
+++ src/main/java/com/example/treinamento_azure/TreinamentoAzureApplication.java
@@ -0,0 +1,13 @@
+package com.example.treinamento_azure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinamentoAzureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinamentoAzureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/treinamento_azure/TreinamentoAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.treinamento_azure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreinamentoAzureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
