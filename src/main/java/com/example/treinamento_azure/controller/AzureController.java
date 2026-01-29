eb67a6e5622f1025813a4e9dcbfaef0c33bab85c
lopes <alexrozaestudos@gmail.com>
2026-01-29 11:26:10 -0300

    wip key-vault : chamada simples para resgatar a key

--- src/main/java/com/example/treinamento_azure/controller/AzureController.java
+++ src/main/java/com/example/treinamento_azure/controller/AzureController.java
@@ -1,16 +1,26 @@
 package com.example.treinamento_azure.controller;
 
+import com.azure.security.keyvault.secrets.SecretClient;
 import com.example.treinamento_azure.model.Mensagem;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class AzureController {
 
+    private final SecretClient secretClient;
 
     @GetMapping("/hello")
     public ResponseEntity<Mensagem> hello() {
         return ResponseEntity.ok(new Mensagem("Hello Azure"));
+    }
+
+    @GetMapping("/key")
+    public ResponseEntity<Mensagem> key() {
+        String secret = secretClient.getSecret("minha-primeira-key").getValue();
+        return ResponseEntity.ok(new Mensagem("Minha secret: " + secret));
     }
 }
